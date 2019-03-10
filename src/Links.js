fea1daa6005508c387fbeccc175777659377c268
Athan Clark <athan.clark@gmail.com>
2019-03-10 14:40:55 -0600

    href button for interlinking

---
+++ src/Links.js
@@ -0,0 +1,9 @@
+"use strict";
+
+exports.attachOnPopStateImpl = function attachOnPopStateImpl (f) {
+    window.addEventListener(
+        'popstate',
+        function onPopStateListenerImpl (e) {f(e.state);},
+        true
+    );
+};
--- src/Links.purs
+++ src/Links.purs
@@ -1,4 +1,4 @@
-module Links where
+module Links (Link (..), linkToDocumentTitle, linkToPathname, linkToHref, pathnameToLink, linkSignal) where
 
 import Links.Bootcamp (BootcampLink (..))
 
@@ -12,10 +12,23 @@ import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Eq (genericEq)
 import Effect (Effect)
 import Effect.Unsafe (unsafePerformEffect)
+import Effect.Uncurried (EffectFn1, runEffectFn1, mkEffectFn1)
 import Web.HTML (window)
-import Web.HTML.Window (location)
-import Web.HTML.Location (protocol, hostname)
-
+import Web.HTML.Window (location, history)
+import Web.HTML.Location (protocol, hostname, pathname, setPathname)
+import Web.HTML.History (DocumentTitle (..), URL (..), replaceState, pushState)
+import Foreign (Foreign, unsafeToForeign, unsafeFromForeign)
+import IxSignal (IxSignal, make, setDiff)
+import Signal.Types (READ, readOnly)
+import Data.TSCompat (OptionRecord)
+import Data.TSCompat.Class (class IsTSEq)
+import MaterialUI.Button (button, ButtonPropsO, ButtonPropsM)
+import React (ReactElement)
+import React.SyntheticEvent (SyntheticMouseEvent, preventDefault)
+import Record (insert) as Record
+import Data.Symbol (SProxy (..))
+import Prim.Row (class Lacks)
+import Unsafe.Coerce (unsafeCoerce)
 
 
 data Link
@@ -34,14 +47,23 @@ rootHostname = do
   pure (p <> "//" <> h)
 
 
+linkToDocumentTitle :: Link -> DocumentTitle
+linkToDocumentTitle l = DocumentTitle $ "USMC Study" <> case l of
+  Bootcamp b -> " - Bootcamp" <> case b of
+    GeneralOrders -> " - General Orders"
+
+
+linkToPathname :: Link -> String
+linkToPathname l = case l of
+  Bootcamp b -> "/bootcamp" <> case b of
+    GeneralOrders -> "/generalOrders"
+
+
 linkToHref :: Link -> String
-linkToHref l =
-  let path = case l of
-        Bootcamp b -> "/bootcamp" <> case b of
-          GeneralOrders -> "/generalOrders"
-  in  host <> path
+linkToHref l = host <> linkToPathname l
   where
     host = unsafePerformEffect rootHostname
+
 
 
 -- | Either just the parsed link, or maybe the closest link requiring redirect
@@ -62,3 +84,56 @@ pathnameToLink p = firstChunk
               then pure (Bootcamp GeneralOrders)
               else Left (Just (Bootcamp GeneralOrders))
         | otherwise -> Left Nothing
+
+
+-- | Redirects on failure to parse
+getLink :: Effect Link
+getLink = do
+  w <- window
+  l <- location w
+  h <- history w
+  p <- pathname l
+  case pathnameToLink p of
+    Right link -> pure link
+    Left mLink -> do
+      let link = case mLink of
+            Nothing -> Bootcamp GeneralOrders
+            Just link' -> link'
+          path = linkToPathname link
+      setPathname path l
+      replaceState (unsafeToForeign link) (linkToDocumentTitle link) (URL path) h
+      pure link
+
+
+foreign import attachOnPopStateImpl :: EffectFn1 (EffectFn1 Foreign Unit) Unit
+
+linkSignal :: Effect (IxSignal (read :: READ) Link)
+linkSignal = do
+  initLink <- getLink
+  sig <- make initLink
+  let go stateF = setDiff (unsafeFromForeign stateF) sig
+  runEffectFn1 attachOnPopStateImpl (mkEffectFn1 go)
+  pure (readOnly sig)
+
+
+hrefButton :: forall a
+            . IsTSEq ({ | a}) (OptionRecord (ButtonPropsO ButtonPropsM) ButtonPropsM)
+           => Lacks "href" a
+           => Lacks "onClick" a
+           => Link
+           -> { | a }
+           -> Array ReactElement
+           -> ReactElement
+hrefButton link ps =
+  let ps' :: { href :: String, onClick :: EffectFn1 SyntheticMouseEvent Unit }
+      ps' = unsafeCoerce $
+        let onClick = mkEffectFn1 \e -> do
+              preventDefault e
+              h <- window >>= history
+              pushState (unsafeToForeign link) (linkToDocumentTitle link) (URL (linkToPathname link)) h
+        in  Record.insert (SProxy :: SProxy "href") (linkToHref link) $
+              Record.insert (SProxy :: SProxy "onClick") onClick ps
+  in  button ps'
+
+
+-- TODO href dropdown, href tab?
