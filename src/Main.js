c26c0b614dd8761b29c316b1bf6c1ebe8ef887e4
Athan Clark <athan.clark@gmail.com>
2019-03-17 12:41:41 -0600

    modernizr

--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,11 @@
 "use strict";
 
 exports.buildDate = buildDate;
+
+exports.hasFlexbox = Modernizr.flexbox;
+
+exports.hasGetrandomvalues = Modernizr.getrandomvalues;
+
+exports.hasHistory = Modernizr.history;
+
+exports.hasTypedarrays = Modernizr.typedarrays;
--- src/Main.purs
+++ src/Main.purs
@@ -11,7 +11,9 @@ import Effect.Console (log)
 import Effect.Exception (throw)
 
 import ReactDOM (render)
-import React (ReactElement, ReactComponent)
+import React (ReactElement, ReactComponent, toElement)
+import React.DOM (text, a)
+import React.DOM.Props (href)
 import Web.HTML (window)
 import Web.HTML.Window (document)
 import Web.HTML.HTMLDocument (toDocument)
@@ -31,13 +33,30 @@ mountToRoot x = do
 foreign import buildDate :: String
 
 
+foreign import hasFlexbox :: Boolean
+foreign import hasGetrandomvalues :: Boolean
+foreign import hasHistory :: Boolean
+foreign import hasTypedarrays :: Boolean
+
+
 main :: Effect Unit
 main = do
   log "Booting up application"
 
   log $ "Build date: " <> buildDate
 
-  windowSizeSignal <- Window.windowSizeSignal
-  linkSignal <- Links.linkSignal
+  let go = do
+        windowSizeSignal <- Window.windowSizeSignal
+        linkSignal <- Links.linkSignal
 
-  void $ mountToRoot $ index windowSizeSignal linkSignal
+        void $ mountToRoot $ index windowSizeSignal linkSignal
+
+  if hasFlexbox && hasGetrandomvalues && hasHistory && hasTypedarrays
+    then go
+    else void $ mountToRoot $ toElement
+           [ text "Unfortunately your Browser doesn't support the features required to run this app. Please upgrade, or consider using "
+           , a [href "https://www.google.com/chrome/"] [text "Google Chrome"]
+           , text " or "
+           , a [href "https://www.mozilla.org/en-US/firefox/"] [text "Mozilla Firefox"]
+           , text "."
+           ]
