1d2b0ef1db636d94f8ee4a83be417d21bcfbbaf3
Athan Clark <athan.clark@gmail.com>
2019-03-20 12:28:31 -0600

    html minifying pipeline

--- build.sh
+++ build.sh
@@ -8,7 +8,12 @@ MODERNIZR=./build/modernizr-custom.js
 
 TEMPLATE=./build/index.template.html
 
+HTMLTMP=./build/index.tmp.html
 OUTPUT=./index.html
+
+BROWSERIFY=./node_modules/.bin/browserify
+HTMLMINIFIER=./node_modules/.bin/html-minifier
+
 
 if [ $# -eq 0 ]; then
     JSOUT=$JS
@@ -22,12 +27,15 @@ fi
 pulp build || { exit 1; }
 purs bundle output/**/*.js -m Main --main Main > $JSTMP || { echo "Bundle Failed"; exit 1; }
 echo "Bundled"
-./node_modules/.bin/browserify $JSTMP -o $JS -t [ babelify --presets [ @babel/preset-env ] --plugins [ @babel/plugin-proposal-class-properties ] ] || { exit 1; }
+$BROWSERIFY $JSTMP -o $JS -t [ babelify --presets [ @babel/preset-env ] --plugins [ @babel/plugin-proposal-class-properties ] ] || { exit 1; }
 rm $JSTMP
 if [ $# -eq 1 ] && [ $1 == "production" ]; then
-    ./node_modules/.bin/browserify $JS -g [ envify --NODE_ENV production ] -g uglifyify | ./node_modules/.bin/uglifyjs --compress --mangle > $JSMIN || { exit 1; }
+    $BROWSERIFY $JS -g [ envify --NODE_ENV production ] -g uglifyify | ./node_modules/.bin/uglifyjs --compress --mangle > $JSMIN || { exit 1; }
 fi
 echo "Browserified"
 date -R | sed 's/\(.*\)/var buildDate = "\1"/g' > $DATE
-ltext "$TEMPLATE $JSOUT $DATE $MODERNIZR" --raw $JSOUT --raw $DATE --raw $MODERNIZR > $OUTPUT || { exit 1; }
+ltext "$TEMPLATE $JSOUT $DATE $MODERNIZR" --raw $JSOUT --raw $DATE --raw $MODERNIZR > $HTMLTMP || { exit 1; }
+echo "Linked"
+$HTMLMINIFIER --collapse-whitespace --collapse-inline-tag-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js false $HTMLTMP > $OUTPUT
+rm $HTMLTMP
 echo "Finished"
--- build/date.js
+++ build/date.js
@@ -1,1 +1,1 @@
-var buildDate = "Wed, 20 Mar 2019 12:07:45 -0600"
+var buildDate = "Wed, 20 Mar 2019 12:28:06 -0600"
